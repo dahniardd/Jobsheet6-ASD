7ac6c69204c969cd3204946e2a4f166b3230c782
dahniardd <dahniardvn09@gmail.com>
2024-03-20 21:07:49 +0700

    Main Hotel Latihan

--- MainHotel06.java
+++ MainHotel06.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 public class MainHotel06 {
     public static void main(String[]args){
         HotelService06 list = new HotelService06();
-        Hotel06 m1 = new Hotel06("Nusa", "Malang", 250000, (byte)3);
+        Hotel06 m1 = new Hotel06("Nusa", "Malang", 250000, (byte) 3);
         Hotel06 m2 = new Hotel06("Rara", "Surabaya", 350000, (byte) 2);
         Hotel06 m3 = new Hotel06("Billy", "Jogja", 125000, (byte) 4);
-        Hotel06 m4 = new Hotel06("Annisa", "Bandung", 400000, (byte)1);
-        Hotel06 m5 = new Hotel06("Nusa", "Jakarta", 500000, (byte)3);
+        Hotel06 m4 = new Hotel06("Annisa", "Bandung", 400000, (byte) 1);
+        Hotel06 m5 = new Hotel06("Nusa", "Jakarta", 500000, (byte) 5);
 
         list.tambah(m1);
         list.tambah(m2);
@@ -19,11 +19,11 @@ public class MainHotel06 {
         System.out.println("Data Harga sebelum sorting = ");
         list.tampil();
 
-        System.out.println("Data Harga setelah sorting menggunakan Bubble Sort = ");
+        System.out.println("Data Harga setelah sorting desc menggunakan Bubble Sort = ");
         list.bubbleSort();
         list.tampil();
 
-        System.out.println("Data Harga setelah sorting menggunakan Selection Sort  = ");
+        System.out.println("Data Rating setelah sorting desc menggunakan Selection Sort  = ");
         list.selectionSort();
         list.tampil();
 
